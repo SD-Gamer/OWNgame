f5e6e6a48d02f0965906902d8f304f939792c789
SD-Gamer <75249081+SD-Gamer@users.noreply.github.com>
2021-03-07 12:02:46 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,54 @@
+class Form{
+    constructor(){
+         this.input = createInput("").attribute("placeholder","name");
+         this.button = createButton("play");
+         this.greeting = createElement("h3");
+         this.title = createElement("h2");
+         this.leaderBoard = createButton("leaderBoard");
+    }
+    display(){
+        this.title.html("Save the Humans");
+        this.title.position(width / 2 - 50,0);
+        this.input.position(width /2 - 40,height /2 - 80);
+        this.button.position(width /2 + 30,height /2);
+        this.button.mousePressed(() => {
+            this.input.hide();
+            this.button.hide();
+            playerName = this.input.value();
+            var curDate = new Date();
+            this.greeting.html("Hello " + playerName);
+            this.greeting.position(width /2 - 70,height /4);
+            var playerIndex = "players/player" + maxId;
+            database.ref(playerIndex).set({
+                name:playerName,
+                date:curDate.getDate() + "/" + curDate.getMonth() + "/" + curDate.getFullYear(),
+                score:0
+            })
+        })
+        this.leaderBoard.position(width /2,20);
+        this.leaderBoard.mousePressed(()=>{
+            playersRef.orderByChild("score").limitToLast(2).on("value",(data) => {
+                allPlayers = data.val();
+                console.log(allPlayers);
+                if(showLeaderBoard){
+                   showLeaderBoard = false;
+                }
+                else{
+                    showLeaderBoard = true;
+                }
+            })
+        })
+        
+    }
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+    }
+    updateScore(finalScore){
+        var playerIndex = "players/player" + maxId;
+        database.ref(playerIndex).update({
+            score:finalScore
+        })
+    }
+}
